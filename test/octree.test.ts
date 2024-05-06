b092f24f7ae62b1747fa840a911396798d6c9f87
Grzegorz Kociołek <gregkociolek@protonmail.com>
2024-05-06 14:27:55 +0200

    tests/octree.test.ts: use id parameter

--- test/octree.test.ts
+++ test/octree.test.ts
@@ -1,16 +1,16 @@
 import * as octree from '@app/octree';
 
 test('Octree constructor', () => {
-	expect(() => new octree.Octree(...Array(9).fill([0]))).toThrow();
+	expect(() => new octree.Octree(0, ...Array(9).fill([0]))).toThrow();
 });
 
 test('Octree.get() sanity', () => {
-	let oct = new octree.Octree([1],[2],[3],[4],[5],[6],[7],[8]);
+	let oct = new octree.Octree(0, [1],[2],[3],[4],[5],[6],[7],[8]);
 	expect(oct.get(0)).toEqual([1]);
 });
 
 test('Octree.get() bound checking', () => {
-	let oct = new octree.Octree();
+	let oct = new octree.Octree(0);
 	expect(()=>oct.get(8)).toThrow();
 	expect(()=>oct.get(-1)).toThrow();
 });
