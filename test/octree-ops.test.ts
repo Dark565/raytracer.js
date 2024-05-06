03243acef4413cfefa648a610e4d286d4d416a57
Grzegorz Kociołek <gregkociolek@protonmail.com>
2024-05-06 14:40:43 +0200

    tests/octree-ops.test.ts: point_at_pos() discrete test

--- test/octree-ops.test.ts
+++ test/octree-ops.test.ts
@@ -3,10 +3,11 @@ import * as ops from '@app/octree_ops';
 import { point } from '@app/linalg';
 
 test('point_at_pos() fuzzy test', ()=>{
-		let otree = new Octree();
+		let otree = new Octree(0);
 		let otree_dim = { pos: point(0,0,0), size: 1 };
 
-		otree.set(0, new Octree);
+		let inner_tree = new Octree(1);
+		otree.set(0, inner_tree);
 
 		let p = point(0,0,0);
 
@@ -16,6 +17,7 @@ test('point_at_pos() fuzzy test', ()=>{
 		}
 
 		for (let i = 0; i < 100; i++) {
+			let expected_node = otree;
 			let rnd = (Math.random()*8)<<0;
 			for (let j = 0; j < 3; j++)
 				p.v[j] = rnd & (1<<j) ? rnd_between(0.5,1.0) : rnd_between(0.0,0.5);
@@ -23,9 +25,21 @@ test('point_at_pos() fuzzy test', ()=>{
 			if (rnd == 0) {
 				let sp = p.scale(1/0.250);
 				rnd = (sp.x <<0) + (sp.y <<1) + (sp.z <<2);
+				expected_node = inner_tree;
 			}
 
 			let node_desc = ops.node_at_pos(otree, otree_dim, p);
-			expect(node_desc).toEqual([otree,rnd]);
+			expect(node_desc).toEqual([expected_node,rnd]);
 		}
+});
+
+test('point_at_pos() discrete tests', ()=>{
+	let otree = new Octree(0);
+	let otree_dim = { pos: point(0,0,0), size: 1 };
+
+	otree.set(3, new Octree(1));
+	otree.get_tree(3).set(5, new Octree(2));
+
+	let node_desc = ops.node_at_pos(otree, otree_dim, point(0.75,0.5,0.25));
+	expect(node_desc).toEqual([otree.get_tree(3).get_tree(5), 0]);
 });
