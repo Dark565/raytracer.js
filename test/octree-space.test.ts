88ce2397adef049b1e40c94caa06597387362d79
Grzegorz Kociołek <gregkociolek@protonmail.com>
2024-05-31 10:51:22 +0200

    tests: adjust octree tests for new octree api

--- test/octree-space.test.ts
+++ test/octree-space.test.ts
@@ -1,18 +1,17 @@
 import { Octree } from '@app/octree';
-import * as ops from '@app/octree_ops';
+import * as space from '@app/octree_space';
 import { point } from '@app/linalg';
 
 test('point_at_pos() fuzzy test', ()=>{
-		let otree = new Octree(0);
-		let otree_dim = { pos: point(0,0,0), size: 1 };
+		const otree_dim = { pos: point(0,0,0), size: 1 };
+		const otree = new Octree(otree_dim);
 
-		let inner_tree = new Octree(1);
-		otree.set(0, inner_tree);
+		const inner_tree = space.new_subtree(otree, 0);
 
-		let p = point(0,0,0);
+		const p = point(0,0,0);
 
-		let rnd_between = (low: number, high: number) => {
-			let delta = high - low;
+		const rnd_between = (low: number, high: number) => {
+			const delta = high - low;
 			return Math.random() * delta + low;
 		}
 
@@ -28,18 +27,18 @@ test('point_at_pos() fuzzy test', ()=>{
 				expected_node = inner_tree;
 			}
 
-			let node_desc = ops.node_at_pos(otree, otree_dim, p);
+			const node_desc = space.node_at_pos(otree, otree_dim, p);
 			expect(node_desc).toEqual([expected_node,rnd]);
 		}
 });
 
 test('point_at_pos() discrete tests', ()=>{
-	let otree = new Octree(0);
-	let otree_dim = { pos: point(0,0,0), size: 1 };
+	const otree_dim: space.OctreeDim = { pos: point(0,0,0), size: 1 };
+	const otree = new Octree(otree_dim);
 
 	otree.set(3, new Octree(1));
 	otree.subtree(3).set(5, new Octree(2));
 
-	let node_desc = ops.node_at_pos(otree, otree_dim, point(0.75,0.5,0.25));
+	let node_desc = space.node_at_pos(otree, otree_dim, point(0.75,0.5,0.25));
 	expect(node_desc).toEqual([otree.subtree(3).subtree(5), 0]);
 });
--- test/octree.test.ts
+++ test/octree.test.ts
@@ -1,12 +1,8 @@
 import * as octree from '@app/octree';
 
 test('Octree constructor', () => {
-	expect(() => new octree.Octree(0, ...Array(9).fill([0]))).toThrow();
-});
-
-test('Octree.get() sanity', () => {
-	let oct = new octree.Octree(0, [1],[2],[3],[4],[5],[6],[7],[8]);
-	expect(oct.get(0)).toEqual([1]);
+	expect(() => new octree.Octree(0, null, ...Array(9).fill([0]))).toThrow();
+	expect(() => new octree.Octree(0, null, ...Array(16).fill([0]))).toThrow();
 });
 
 test('Octree.get() bound checking', () => {
