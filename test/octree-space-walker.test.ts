611c7c3bfba2831f5634c5e3afef58044d93c8cb
Grzegorz Kociołek <gregkociolek@protonmail.com>
2024-06-01 12:05:45 +0200

    tests update

--- test/octree-space-walker.test.ts
+++ test/octree-space-walker.test.ts
@@ -2,18 +2,27 @@ import * as linalg from '@app/linalg';
 import * as otree from '@app/octree';
 import * as space from '@app/octree_space';
 
+/** Tests only one level of the tree */
+function walker_one_level_test<T>(walker: space.OctreeWalker<T>, pos: linalg.Point, dir: linalg.Vector, expected_crosses: number[]) {
+	walker.start_point = pos;
+	walker.direction = dir;
+	const crosses = Array.from(walker.each_cross({include_undefined: true}));
+	expect(crosses.map(x => x[1])).toEqual(expected_crosses);
+}
+
 test('OctreeWalker sanity', () => {
 	const dim: space.OctreeDim = { pos: linalg.point(0,0,0), size: 1 };
 	const tree = new otree.Octree<any,space.OctreeDim>(dim);
 	const tree_walker = new space.OctreeWalker(tree);
-	tree_walker.start_point = linalg.point(0.1,0.1,0.1);
-	tree_walker.direction = linalg.vector(3/4, Math.sqrt(3)/4, 0);
-	//tree_walker.direction = linalg.vector(0, 1, 0);
-	
-	let time_start = performance.now();
-	for (let sub of tree_walker.each_cross({include_undefined: true})) {
-		const time_now = performance.now();
-		console.log(`delta ${(Number(time_now) - Number(time_start)) / 1000}: crossing child ${sub[1]}`);
-		time_start = performance.now();
-	}
+
+	walker_one_level_test(tree_walker, linalg.point(0.1,0.1,0.1), linalg.vector(3/4, Math.sqrt(3)/4, 0), [0,1,3]);
+	walker_one_level_test(tree_walker, linalg.point(0.1,0.1,0.1), linalg.vector(1,1,1), [0,2,3,7]);
+
+	//let time_start = performance.now();
+	//for (let sub of tree_walker.each_cross({include_undefined: true})) {
+	//	const time_now = performance.now();
+	//	console.log(`delta ${(Number(time_now) - Number(time_start)) / 1000}: crossing child ${sub[1]}`);
+	//	time_start = performance.now();
+	//}
+
 });
